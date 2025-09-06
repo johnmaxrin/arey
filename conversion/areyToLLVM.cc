508cdf045651b106a9d1eee6b8d03e890cfd8818
60b36t <johnmaxrin@gmail.com>
2025-09-06 17:56:14 +0530

    Add Op Patterns

--- conversion/areyToLLVM.cc
+++ conversion/areyToLLVM.cc
@@ -1,15 +1,36 @@
-#include "mlir/Dialect/SCF/IR/SCF.h"
-#include "mlir/Dialect/MemRef/IR/MemRef.h"
-#include "mlir/Dialect/Arith/IR/Arith.h"
-#include "mlir/Dialect/Func/IR/FuncOps.h"
-#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
-
 
 #include "areyToLLVM.h"
 #include "dialect/Passes.h.inc"
 
 using namespace mlir;
 using namespace mlir::arey;
+
+
+struct ConvertPrintToLLVM : public OpConversionPattern<PrintOp>
+{
+    using OpConversionPattern::OpConversionPattern;
+
+    LogicalResult matchAndRewrite(
+        PrintOp op, OpAdaptor adaptor,
+        ConversionPatternRewriter &rewriter) const override
+    {
+        rewriter.eraseOp(op);
+        return success();
+    }
+};
+
+struct ConvertPrintStringToLLVM : public OpConversionPattern<PrintStringOp>
+{
+    using OpConversionPattern::OpConversionPattern;
+
+    LogicalResult matchAndRewrite(
+        PrintStringOp op, OpAdaptor adaptor,
+        ConversionPatternRewriter &rewriter) const override
+    {
+        rewriter.eraseOp(op);
+        return success();
+    }
+};
 
 namespace{
     #define GEN_PASS_DEF_CONVERTAREYTOLLVMPASS
@@ -21,7 +42,23 @@ namespace{
             using ConvertAreyToLLVMPassBase::ConvertAreyToLLVMPassBase;
 
             void runOnOperation() override {
-                llvm::errs() << "Hello";
+                mlir::MLIRContext *context = &getContext();
+                auto *module = getOperation();
+
+                ConversionTarget target(*context);
+                target.addIllegalOp<arey::PrintOp>();
+                target.addIllegalOp<arey::PrintStringOp>();
+                target.markUnknownOpDynamicallyLegal([](Operation *op) { return true; });
+            
+                
+                RewritePatternSet patterns(context);
+
+                patterns.add<ConvertPrintToLLVM, ConvertPrintStringToLLVM>(context);
+                if (failed(applyPartialConversion(module, target, std::move(patterns))))
+                {
+                    signalPassFailure();
+                }
+                
             }
         };
 }
--- conversion/areyToLLVM.h
+++ conversion/areyToLLVM.h
@@ -1,6 +1,19 @@
 #ifndef AREY_TO_LLVM
 #define AREY_TO_LLVM
 
+
+#include "mlir/Dialect/SCF/IR/SCF.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+
+#include "mlir/Transforms/DialectConversion.h"
+#include "mlir/Conversion/LLVMCommon/ConversionTarget.h"
+
+#include "includes/areyOps.h"
+
+
 #include "mlir/Pass/Pass.h"
 
 #include "includes/areyDialect.h"
